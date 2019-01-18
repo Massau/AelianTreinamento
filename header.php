c64ae3d7996699ae152fd11fdfe9968b220ae9ed
Massau <joyce.santos202@etec.sp.gov.br>
2019-01-18 21:27:14 -0200

    Criar arquivo de cabeçalho.php

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>		
+	<title>Mentoria</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="shortcut icon" href="imagens/favicon.ico" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+</head>
+
+<body>
