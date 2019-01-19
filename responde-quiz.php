5310f86f36a1f4e17d2b5a4b62d39bdf2acf54ac
Massau <joyce.santos202@etec.sp.gov.br>
2019-01-19 15:50:18 -0200

    Criar função que Insere resposta

--- responde-quiz.php
+++ responde-quiz.php
@@ -1,11 +1,14 @@
 <?php include("header.php");
+function insereResposta($conexao, $nome, $idade){
+    $query = "insert into quiz (nome, idade) values ('{$nome}', {$idade})";
+    return mysqli_query($conexao, $query);
+}
 
 $nome = $_GET['nome'];
 $idade = $_GET['idade'];
 $conexao = mysqli_connect('localhost', 'root', '', 'crud');
 
-$query = "insert into quiz (nome, idade) values ('{$nome}', {$idade})";
-if(mysqli_query($conexao, $query)){ ?>
+if(insereResposta($conexao, $nome, $idade)){ ?>
     <p class="alert-success text-center">Questionário respondido com sucesso, <?= $nome?>, de <?= $idade ?> anos</p>
 <?php } else { ?>
     <p class="alert-danger text-center">Erro no envio das respostas <br> Verifique se todos os campos foram preenchidos corretamente</p>
