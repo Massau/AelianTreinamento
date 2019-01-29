ff10ee9a359b31809494c3bd771f131102d718ab
Massau <joyce.santos202@etec.sp.gov.br>
2019-01-29 20:35:40 -0200

    Adiciona arquivos do backup para corrigir quebra de layout

---
+++ adiciona-produto.php
@@ -0,0 +1,21 @@
+<?php 
+    include("cabecalho.php"); 
+    require_once("conecta.php");
+    require_once("banco-respostas.php");
+
+$nome = $_GET['nome'];
+$idade = $_GET['idade'];
+
+if(insereResposta($conexao, $nome, $idade)){ ?>
+    <p class="text-success text-center envolve_conteudo">Questionário respondido com sucesso, <?= $nome?>, de <?= $idade ?> anos</p>
+<?php } else { 
+    $msgErro = mysqli_error($conexao);
+?>
+    <p class="text-danger text-center envolve_conteudo">Erro no envio das respostas: <?= $msgErro ?></p>
+<?php
+}
+
+mysqli_close($conexao);
+?>
+
+<?php include("rodape.php"); ?>
---
+++ rodape.php
@@ -0,0 +1,17 @@
+	<footer class="container-fluid bg-dark text-center p-t-8 p-b-8">
+	<br>
+		<img src="imagens/logoPB.png">
+		<a href="index.html">Joyce Massau</a>
+	</footer>
+
+    <script src="js/scripts.js"></script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
