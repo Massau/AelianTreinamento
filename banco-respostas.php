066fef5b72ff17e1ab0626d53afed8edd735baac
Massau <joyce.santos202@etec.sp.gov.br>
2019-01-29 21:47:16 -0200

    Permite que usuário remova respostas da listagem de respostas

--- banco-respostas.php
+++ banco-respostas.php
@@ -3,7 +3,7 @@
 function listaRespostas($conexao) {
     $respostas = array();
     $resultado = mysqli_query($conexao, "select * from quiz");
-    while($resposta = mysqli_fetch_assoc($resultado)){
+    while($resposta = mysqli_fetch_assoc($resultado)) {
         array_push($respostas, $resposta);
     }
     return $respostas;
@@ -11,6 +11,11 @@ function listaRespostas($conexao) {
 
 function insereResposta($conexao, $nome, $idade){
     $query = "insert into quiz (nome, idade) values ('{$nome}', {$idade})";
+    return mysqli_query($conexao, $query);
+}
+
+function removeResposta($conexao, $id) {
+    $query = "delete from quiz where id = {$id}";
     return mysqli_query($conexao, $query);
 }
 ?>
--- remove-resposta.php
+++ remove-resposta.php
@@ -1,4 +1,12 @@
 <?php 
     include("cabecalho.php"); 
     require_once("conecta.php");
-    require_once("banco-respostas.php"); ?>
+    require_once("banco-respostas.php"); 
+
+    $id = $_GET['id'];
+    removeResposta($conexao, $id);
+    ?>
+
+    <div class="container mt-1">
+    <p class="text-success text-center envolve_conteudo">Resposta removida com sucesso</p>
+    </div>
