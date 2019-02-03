7c41bef66d3812085ce27728d099accd574344cd
Massau <joyce.santos202@etec.sp.gov.br>
2019-02-03 19:46:34 -0200

    Acrescenta funcionalidade de alterar respostas

--- altera-resposta-formulario.php
+++ altera-resposta-formulario.php
@@ -1,47 +1,29 @@
 <?php include("cabecalho.php");
       include("conecta.php");
-      /*include("banco-categoria.php");*/
-      include("banco-produto.php");
+      include("banco-respostas.php");
 
 $id = $_GET['id'];
-$produto = buscaProduto($conexao, $id);
-
-$categorias = listaCategorias($conexao);
-
-$usado = $produto['usado'] ? "checked='checked'" : "";
+$resposta = buscaResposta($conexao, $id);
 ?>
 
-<h1>Alterando produto</h1>
-<form action="altera-resposta.php" method="post">
-    <input type="hidden" name="id" value="<?=$resposta['id']?>" />
-    <table class="table">
-        <tr>
-            <td>Nome</td>
-            <td><input class="form-control" type="text" name="nome" value="<?=$resposta['nome']?>" /></td>
-        </tr>
-        <tr>
-            <td>Idade</td>
-            <td><input class="form-control" type="number" name="idade" value="<?=$resposta['preco']?>" /></td>
-        </tr>
-        <tr>
-            <td>Categoria</td>
-            <td>
-                <select class="form-control" name="categoria_id">
-                    <?php foreach($categorias as $categoria) :
-                        $essaEhACategoria = $produto['categoria_id'] == $categoria['id'];
-                        $selecao = $essaEhACategoria ? "selected='selected'" : "";
-                    ?>
-                        <option value="<?=$categoria['id']?>" <?=$selecao?>>
-                            <?=$categoria['nome']?>
-                        </option>
-                    <?php endforeach ?>
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td><button class="btn btn-primary" type="submit">Alterar</button></td>
-        </tr>
-    </table>
-</form>
+<div class="container pt-5 mt-5">
+    <h1>Alterando produto</h1>
+        <form action="altera-resposta.php" method="post">
+            <input type="hidden" name="id" value="<?=$resposta['id']?>" />
+            <table class="table pt-3 mt-3">
+                <tr>
+                    <td>Nome</td>
+                    <td><input class="form-control" type="text" name="nome" value="<?=$resposta['nome']?>" /></td>
+                </tr>
+                <tr>
+                    <td>Idade</td>
+                    <td><input class="form-control" type="number" name="idade" value="<?=$resposta['idade']?>" /></td>
+                </tr>
+                <tr>
+                    <td><button class="btn btn-primary" type="submit">Alterar</button></td>
+                </tr>
+            </table>
+        </form>
+</div>
 
 <?php include("rodape.php"); ?>
--- banco-respostas.php
+++ banco-respostas.php
@@ -19,6 +19,12 @@ function alteraResposta($conexao, $id, $nome, $idade) {
     return mysqli_query($conexao, $query);
 }
 
+function buscaResposta($conexao, $id) {
+    $query = "select * from quiz where id = {$id}";
+    $resultado = mysqli_query($conexao, $query);
+    return mysqli_fetch_assoc($resultado);
+}
+
 function removeResposta($conexao, $id) {
     $query = "delete from quiz where id = {$id}";
     return mysqli_query($conexao, $query);
