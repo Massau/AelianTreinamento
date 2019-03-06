bcc8efbbde11fcf59bb532d7e6365fabdbd8b642
Massau <joyce.santos202@etec.sp.gov.br>
2019-03-06 16:18:28 -0300

    Referencia arquivos jQuery e não mais seu CDN

--- rodape.php
+++ rodape.php
@@ -8,7 +8,7 @@
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/parallax/3.1.0/parallax.min.js"></script>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/jquery.js"></script>
     
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     
